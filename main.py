6ffea19af1003726b16bf858eb7bd85767df8b0e
Alycecodex <alycecodex@hotmail.com>
2026-07-16 20:42:22 +0100

    Update main.py
    my code alyce

--- main.py
+++ main.py
@@ -16,23 +16,22 @@ import os
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
-
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+date = dt.datetime.now()
+month = date.month
+day = date.day
+
+birthday_dict = {(data_row.month,data_row.day):data_row for (new,data_row) in data.iterrows()}
+if (month,day) in birthday_dict:
+    birthday_person = birthday_dict[(month,day)]
+    with open(f"letter_templates/letter_{random.randint(1,3)}.txt") as f:
+        letter_template = f.read()
+        n = letter_template.replace("[NAME]",birthday_person["name"])
+        print(n)
+
+    with smtplib.SMTP("smtp.gmail.com",587) as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        connection.login(my_email,password)
+        connection.sendmail(from_addr=my_email,to_addrs=birthday_person["email"],msg=f"Subject: Happy Birthday!\n\n{n}")
+
